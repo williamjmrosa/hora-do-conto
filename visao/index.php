9d475a9506037a98a3c20f2ca3338f2d70caf279
William José <williamjmrosa@gmail.com>
2022-04-18 12:25:00 -0300

    ADD pagina Home incompleta

---
+++ visao/index.php
@@ -0,0 +1,42 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+<title>Hora do Conto</title>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport"
+	content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="../Framework/css/bootstrap.min.css">
+<link rel="stylesheet" href="../CSS/styles.css">
+<link rel="icon" type="image/jpg" href="../img/Logo1.png" />
+</head>
+<body>
+<div id="menu_superior">
+<div id="logo1"><img alt="Hora do Conto" src="../img/Logo1.png"></div>
+<div id="menu">
+		<a href="../visao/sobre.php">Sobre Nós</a>
+		<a href="../visao/sorvicos.php">Serviços</a>
+		<a href="../visao/contato.php">Contato</a>
+</div>
+<div id="menu2">
+	<a href="../visao/cadastro.php">Cadastre-se</a>
+	<a href="../visao/cadastro.php">Acessar</a>
+</div>
+
+</div>
+<div id="artigo">
+	<ul class="check-list">
+		<li>Conteúdo online</li>
+		<li>Material didático disponibilizado por professores</li>
+		<li>Exercícios baseados nos contos infantis</li>
+	</ul>
+	<img alt="Estudo" src="../img/aluna.jpeg">
+</div>
+<script src="../Framework/js/jquery-3.3.1.slim.min.js"></script>
+	<script src="../Framework/js/popper.min.js"></script>
+	<script src="../Framework/js/bootstrap.min.js"></script>
+</body>
+</html>
