ce5383f5ed52d6009742a32dec8a74753012b8cc
William José <williamjmrosa@gmail.com>
2022-04-29 19:45:18 -0300

    Index feito até planos

--- visao/index.php
+++ visao/index.php
@@ -9,34 +9,106 @@
 <meta charset="utf-8">
 <meta name="viewport"
 	content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="stylesheet" href="../Framework/css/bootstrap.min.css">
 <link rel="stylesheet" href="../CSS/styles.css">
 <link rel="icon" type="image/jpg" href="../img/Logo1.png" />
+
+<!-- Bootstrap CSS-->
+<link rel="stylesheet" href="../Framework/css/bootstrap.min.css">
+
 </head>
 <body>
-<div id="menu_superior">
-<div id="logo1"><img alt="Hora do Conto" src="../img/Logo1.png"></div>
-<div id="menu">
-		<a href="../visao/sobre.php">Sobre Nós</a>
-		<a href="../visao/sorvicos.php">Serviços</a>
-		<a href="../visao/contato.php">Contato</a>
-</div>
-<div id="menu2">
-	<a href="../visao/cadastro.php">Cadastre-se</a>
-	<a href="../visao/cadastro.php">Acessar</a>
-</div>
+	<header class="p-3 primaria fonte mb-2">
+		<!-- Menu Superior -->
+		<div class="m-0">
+			<div
+				class="d-flex flex-wrap align-items-center justify-content-center">
+				<a href="index.php"
+					class="d-flex align-items-center mb-2 mb-lg-0 text-decoration-none">
+					<img class="me-4 tamanho" alt="Hora do conto"
+					src="../img/logo1.png">
+				</a>
 
-</div>
-<div id="artigo">
-	<ul class="check-list">
-		<li>Conteúdo online</li>
-		<li>Material didático disponibilizado por professores</li>
-		<li>Exercícios baseados nos contos infantis</li>
-	</ul>
-	<img alt="Estudo" src="../img/aluna.jpeg">
-</div>
-<script src="../Framework/js/jquery-3.3.1.slim.min.js"></script>
+				<ul
+					class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+					<li><a class="fonte me-3" href="../visao/sobre.php">Sobre Nós</a>
+					
+					<li><a class="fonte me-3" href="../visao/sorvicos.php">Serviços</a></li>
+					<li><a class="fonte me-3" href="../visao/contato.php">Contato</a></li>
+				</ul>
+
+				<div class="text-end">
+					<a class="btn secundaria me-3 fonte">Cadastre-se</a> <a
+						class="btn secundaria fonte">Acessar</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div id="artigo" class="d-flex flex-wrap primaria mb-4">
+		<div class="mt-4 ms-5">
+			<ul class="check-list">
+				<li>Conteúdo online</li>
+				<li>Material didático disponibilizado por professores</li>
+				<li>Exercícios baseados nos contos infantis</li>
+
+			</ul>
+			<a class="btn secundaria fonte p-2 ms-4 ps-5 pe-5">SAIBA MAIS</a>
+		</div>
+		<img class="container" alt="Estudo" src="../img/aluna.jpeg">
+	</div>
+	<div id="planos" class="fundo">
+		<h1 class="fonte text-primaria m-3">NOSSOS PLANOS</h1>
+		<div id="cardes" class="cards mt-4 d-lg-flex flex-row justify-content-between align-items-center container m-0">
+			<!-- Card Aluno -->
+			<div class="card fundo mt-4">
+				<div id="aluno" class="card-body primaria">
+					<h4 class="card-title fonte">ALUNOS</h4>
+					<ul class="list-card">
+						<li class="fonte">Acesso aos vídeos e exercícios</li>
+						<li class="fonte">Score de desempenho</li>
+						<li class="fonte">Benefícios do Clube do Conto</li>
+					</ul>
+					<h3 class="card-text text-end">R$ 49,90</h3>
+					<a href="#" class="w-100 p-3 btn secundaria fonte">COMECE AGORA</a>
+				</div>
+				
+			</div>
+			<!-- Card Professor -->
+			<div class="card fundo mt-4">
+				<div id="professor" class="card-body primaria">
+					<h4 class="card-title fonte">PROFESSORES</h4>
+					<ul class="list-card">
+						<li class="fonte">Acesso aos vídeos e exercícios</li>
+						<li class="fonte">Upload de vídeos</li>
+						<li class="fonte">Acesso a ferramenta auxiliar para elaboração de exercícios</li>
+					</ul>
+					<h3 class="card-text text-end">R$ 39,90</h3>
+					<a href="#" class="w-100 p-3 btn secundaria fonte">COMECE AGORA</a>
+				</div>
+				
+			</div>
+			<!-- Instituição -->
+			<div class="card fundo mt-4">
+				<div id="instituicao" class="card-body primaria">
+					<h4 class="card-title fonte">INSTITUIÇÕES</h4>
+					<ul class="list-card">
+						<li class="fonte">Acesso aos vídeos e exercícios</li>
+						<li class="fonte">Chaves de acesso para alunos</li>
+						<li class="fonte">Benefícios do Clube do Conto</li>
+					</ul>
+					<h3 class="card-text text-end">R$ 99,90</h3>
+					<a href="#" class="w-100 p-3 btn secundaria fonte">COMECE AGORA</a>
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+	<div id="sobre" class="fundo">
+	<h1 class="fonte text-primaria m-3">Sobre a Hora do Conto</h1>
+	</div>
+	<!-- JavaScript -->
 	<script src="../Framework/js/popper.min.js"></script>
 	<script src="../Framework/js/bootstrap.min.js"></script>
+	<!-- JQuery -->
+	<script src="../Framework/jquery-3.6.0.min.js"></script>
 </body>
-</html>
