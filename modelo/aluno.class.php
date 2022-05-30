43bddf50f16f83b12258b9a2d470c938140805e5
William José <williamjmrosa@gmail.com>
2022-05-30 08:06:57 -0300

    Classe Aluno

---
+++ modelo/aluno.class.php
@@ -0,0 +1,35 @@
+<?php
+Class Aluno{
+    
+    //Atributo
+    private $cpf_cnpj;
+    private $nome;
+    private $email;
+    private $contato;
+    private $senha;
+    
+    //Construtor
+    public function __construct() {
+    }//fecha construtor
+    
+    //Método GET
+    public function __get($a) {
+        return $this->$a;
+    }
+    
+    //Método SET
+    public function __set($a,$v) {
+        $this->$a = $v;
+    }
+    
+    //Método toString
+    public function __toString(){
+        return nl2br("CPF Aluno: $this->cpf_cnpj
+                        Nome: $this->nome
+                        E-mail: $this->email
+                        Contato: $this->contato
+                        Senha: $this->senha");
+    }//fecha toString
+    
+}//fecha classe
+?>
