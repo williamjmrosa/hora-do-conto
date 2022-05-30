37157020c9f414613d134684938dd0d5e05a40db
William José <williamjmrosa@gmail.com>
2022-05-30 16:17:47 -0300

    Verificar tipo quando fazer o submit

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,17 +1,26 @@
 /**
  * 
  */
-$("select").change(function(){
+$("#tipo").change(function(){
 	var tipo = $(this).val();
 	if(tipo == 1){
-		$("#formulario").attr('action','../responsavel-controle.php?op=1');
+		$("#formulario").attr('action','../controle/responsavel-controle.php?op=1');
 	}else if(tipo == 2){
-		$("#formulario").attr('action','../aluno-controle.php?op=1');
+		$("#formulario").attr('action','../controle/aluno-controle.php?op=1');
 	}else if(tipo == 3){
-		$("#formulario").attr('action','../professor-controle.php?op=1');
+		$("#formulario").attr('action','../controle/professor-controle.php?op=1');
 	}else if(tipo == 4){
-		$("#formulario").attr('action','../instituicao-controle.php?op=1');
+		$("#formulario").attr('action','../controle/instituicao-controle.php?op=1');
 	}else{
 		$("#formulario").attr('action','');
 	}
+});
+
+$("#formulario").submit(function(){
+	var tipo = $("select").val();
+	if(tipo == 'Selecione'){
+		alert("Selecione um tipo!");
+		return false;
+	}
+	
 });
