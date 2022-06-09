e524e2fb1013c6084aeb6c9cea3bec99f78d5267
William José <williamjmrosa@gmail.com>
2022-06-09 17:59:08 -0300

    JavaScript Colocando a Mask para Contato e CPF ou CNPJ dependo do cli

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,6 +1,23 @@
 /**
  * 
  */
+ 
+$(document).ready(function(){
+	 $("#contato").mask('(00) 00000-0000');
+	 
+	var options =  {
+  onKeyPress: function(cep, e, field, options) {
+    var tipo = $("#tipo").val();
+    var masks = ['000.000.000-00', '00.000.000/0000-00'];
+    var mask = ( tipo == 4) ? masks[1] : masks[0];
+    
+    $('#cpf-cnpj').mask(mask, options);
+}};
+
+$('#cpf-cnpj').mask('000.000.000-00', options);
+	
+});
+ 
 $("#tipo").change(function(){
 	var tipo = $(this).val();
 	if(tipo == 1){
