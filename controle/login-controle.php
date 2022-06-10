e12ecfc39de67134a2296203f2cdce84d6650f1f
William José <williamjmrosa@gmail.com>
2022-06-09 22:57:43 -0300

    Login controle que se conecta com a classe ControleLogin

---
+++ controle/login-controle.php
@@ -0,0 +1,47 @@
+<?php
+session_start();
+include '../util/seguranca.class.php';
+include '../util/controle-login.class.php';
+include '../modelo/aluno.class.php';
+include '../modelo/professor.class.php';
+
+if (isset($_GET['op'])) {
+	$OP = filter_var(@$_REQUEST['op'],FILTER_SANITIZE_NUMBER_INT);
+	
+	switch ($OP) {
+		//Login
+		case 1:
+			$email = filter_var(@$_POST['email'],FILTER_SANITIZE_STRING);
+			$senha = filter_var(@$_POST['senha'],FILTER_SANITIZE_STRING);
+			$tipo = filter_var(@$_POST['tipoLogin'],FILTER_SANITIZE_NUMBER_INT);
+			$senha = Seguranca::criptografar($senha);
+			$cliente = null;
+			
+			if($tipo == 1){
+				
+			}else if($tipo == 2){
+				$cliente = new Aluno;
+			}else if($tipo == 3){
+				$cliente = new Professor;
+			}else{
+				
+			}
+			$cliente->email = $email;
+			$cliente->senha = $senha;
+			
+			ControleLogin::logar($cliente, $tipo);
+		break;
+		
+		case 2:
+			ControleLogin::deslogar();
+		break;
+		
+		default:
+			header('location:../visao/index.php');
+		break;
+	}
+}else{
+	header('location:../visao/index.php');
+}
+
+?>
